f89aa85f1d996edad1b53dfc7aa466b219732030
Mounika420 <148519406+Mounika420@users.noreply.github.com>
2023-10-29 21:21:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,21 +18,6 @@ resource "aws_instance" "multiple_applications" {
     user = "ubuntu"
     private_key = file("keypair")    
  }
- provisioner "remote-exec" {
-  inline = [
-            "apt install unzip",
-            "adduser sonarqube",
-            "wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip -y",
-            "unzip *",
-            "chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424",
-            "chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424",
-            "cd sonarqube-9.4.0.54424/bin/linux-x86-64/",
-            "./sonar.sh start"           
-  ] 
-    }  
-    
-  }
-
 #Create the keypair the  of applications
 resource "aws_key_pair" "tf-key-pair" {
 key_name = "keypair"
